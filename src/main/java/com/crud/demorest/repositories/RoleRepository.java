b1fdd8bf532aa094dc299f1b96111bc936775200
Pavel Lamov <git.habov@yandex.ru>
2021-08-20 05:33:39 +0300

    Initial commit

---
+++ src/main/java/com/crud/demorest/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.crud.demorest.repositories;
+
+import com.crud.demorest.entitys.Role;
+import com.crud.demorest.entitys.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+
+}
---
+++ src/main/java/com/crud/demorest/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.crud.demorest.repositories;
+
+import com.crud.demorest.entitys.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+}
